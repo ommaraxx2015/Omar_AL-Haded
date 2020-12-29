af225f6f63beb0ee70079aa6f8e775a0d4c4a202
ommaraxx2015 <76623269+ommaraxx2015@users.noreply.github.com>
2020-12-29 23:45:29 +0200

    Create AL-Haded.js

---
+++ AL-Haded.js
@@ -0,0 +1,23 @@
+$("<div class='fr borderg' style='padding:2px;background-color:white;float:left;'> استايلات و ارشفة :<a href=''>عمر الحديد</a></div>").insertAfter($(".loginstat"));
+$('.nav-tabs').addClass('fl').css('width', '100%');
+$(`<link rel="stylesheet" type="text/css" href="">
+<link rel="stylesheet" href="https://rawcdn.githack.com/ommaraxx2015/Omar_AL-Haded/18dcbf1f7784dc555d6f4f5ddf53374b7404506c/AL-Haded.css">
+</style>`).insertBefore('body');
+$(".fa-gear").text("الضبط").css("width","");
+(document.getElementById("tbox").placeholder = "( مـن يلـوم القـلب لعـشـق مغربيـة )");
+$("[data-target=#wall]").attr('title','       شـــات احبـك  للـجــوال    ')
+$("[data-target=#usearch]").attr('title','    شـــات احبـك  للـجــوال       ')
+$("[data-target=#settings]").attr('title','     شـــات  احبـك للـجــوال')
+$("[data-target=#rooms]").attr('title','   شـــات  احبـك للـجــوال ')
+$("[data-target=#chats]").attr('title','      شـــات  احبـك للـجــوال  ')
+$("[data-target=#users]").attr('title','  شـــات احبـك  للـجــوال ')
+$ ( ` <center> <div style =" width: 100٪؛ height: 0%! important؛ "id =" as "margin: 2px؛" class = "fa fa-heartbeat btn btn-primary" type = "button" data-toggle = "collapse" data-target = "#apseExample" aria-expanded = "false" aria-controls = "collapseExample"> ( شـــات  احبـك للـجــوال ) َ</ div> <div class = "" id = "collapseExample"> <center> </ i> </ div> <center> ` ). insertBefore ( '#d2' );
+$(`<div><marquee direction="right" width="100%" id="AL-Haded0" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="4"> حييياكم شششات  احبـك  للجوال اول رابط بقوقل  ... </marquee></div>`).insertBefore('div#l1');
+$('.u-msg.dots').removeClass('dots');$(".ubnr").remove();$(".dpnl").css('width','310px');$("div#l1 button.btn.btn-primary").bind("click",function(){alert("اهلا وسهلا بكم في شات آحـبك  سجل معنا عضويه لتحصل على لايكات ولدايه وزخرفه وحفظ بروفايلك وكل عام وانتم بخير سنه سعيده عليكم 2021");})
+if(location.protocol!='https:'){location.protocol='https:';}
+$(`<label onclick="window.open('cp?token='+token,'_blank');" style="background-color: ghostwhite;color: black;margin:4px; padding:8px;width:98%;" class="label tc border cp  btn  label-danger fl"><span class="fl fa fa-star"></span>ادارة بيانات المواقع</label>`).insertAfter('#settings label.label.tc.border.btn.fl[onclick="sendpic();"]');Tsend_=Tsend;reqlikes=5;Tsend=function(){if(getuser(myid).rep<reqlikes){alert(' لإ يمكنك الكتابة عام يتطلب منك الحصول على لايكات'+reqlikes+' ');return;}else{Tsend_();}}
+setprofile_=setprofile;reqlikes=5;setprofile=function(){if(getuser(myid).rep<reqlikes){alert('لا يمكنك حفظ الأعدادات يتطلب منك الحصول على لايكات '+reqlikes+'');return;}else{setprofile_();}}
+$("div#l2 button.btn.btn-primary").bind("click",function(){alert("اهلا وسهلا بكم في عائلتنا عائلة شات احبك للجوال ");})
+$("div#l3 button.btn.btn-primary").bind("click",function(){alert("عند تسجيلك للعضوية فانك توافق على عدم قوانين الشات واي اختلال في القوانين سيتم طردك صاحبة الشات رانيا  المغربية وصاحب الشات عمر الحديد وزياد الشمري");})
+$('.checkbox label').html(`<input id="stealth" type="checkbox" value="" maxlength="10"><i class="fa fa-eye-slash" aria-hidden="true">`)
+$(`<center><div><div width="99.5%" id="" /font/div/div><center>`).insertBefore('#d2');$(`<div style=" color: #FC1717;text-align: center;margin-bottom: 10px; font-family:'jazeera-light', Fontawesome">لُطفاً ♥️ الإعلانات للضروره فقط </div>`).insertBefore('span.fl.fa.fa-send');
